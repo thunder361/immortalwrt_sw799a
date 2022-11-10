22b6065dfab7aa92ef023bbebb4aaf7643376f99
yuqi <34715934+mrlj147@users.noreply.github.com>
2022-11-10 13:15:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,5 +28,10 @@ svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome pa
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git helloworld https://github.com/sbwml/openwrt_helloworld' >>feeds.conf.default
-echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/sbwml/openwrt_helloworld' >>feeds.conf.default
+# echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
+
+# openwrt_helloworld
+svn co  https://github.com/sbwml/openwrt_helloworld package/helloworld
+# mosdns
+svn co  https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
